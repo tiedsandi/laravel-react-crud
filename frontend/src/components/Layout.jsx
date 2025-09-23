6bc4accd4ea7d29e63a3cf4f1dec9173440c8bfc
tiedsandi <fachransandi@gmail.com>
2025-09-23 11:57:46 +0700

    feat: update Link Logo

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,10 +1,12 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export default function EmployeeLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-gray-100">
-      <header className="bg-blue-600 text-white py-6">
-        <h1 className="text-2xl font-bold text-center">Employee Management</h1>
+      <header className="bg-blue-600 text-white py-6 text-center">
+        <Link to={"/"} className="text-2xl font-bold uppercase">
+          Employee Management
+        </Link>
       </header>
 
       <main className="flex-grow flex justify-center items-start py-8 px-4">
